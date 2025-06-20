8f2e599522d2ac0da7803b811edcbaebd1d327db
Harmanpreet Singh <harmanpreet181001@gmail.com>
2025-06-20 20:43:35 +0530

    feat: added jwt service

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -3,10 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User])
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+        secret: "secretKey",
+        signOptions: {
+            expiresIn: "1h"
+        }
+    })
   ],
   controllers: [AuthController],
   providers: [AuthService],
